9ca30d628d7fade858e6e725fabf8f3b11757b44
Greg Brosman <greg.brosman@gmail.com>
2020-07-31 20:46:06 -0700

    uniques answer

--- 6kyu/unique_in_order.py
+++ 6kyu/unique_in_order.py
@@ -7,13 +7,24 @@
 # unique_in_order([1,2,2,3,3])       == [1,2,3]
 
 
+# def unique_in_order(iterable):
+#     uniques_list = []
+#     for x in iterable:
+#         if x not in uniques_list:
+#             uniques_list.append(x)
+#     print(uniques_list)
+
+
+# Append the char to the list if not already in the list, and if char does not equal the previous. 
+# Make variable prev equal to the char that was just added so that it isn't added again.
 def unique_in_order(iterable):
-    uniques_list = []
-    for x in iterable:
-        if x not in uniques_list:
-            uniques_list.append(x)
-    print(uniques_list)
+    result = []
+    prev = None
+    for char in iterable:
+        if char != prev:
+            result.append(char)
+            prev = char
+    print(result)
 
-
-unique_in_order('AAAABBBCCDAABBB')
+unique_in_order('ABBCcAD')
         
