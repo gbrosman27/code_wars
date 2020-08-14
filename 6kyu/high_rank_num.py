7f574a77e6811086015e2aa0f5d639c7dc86de6d
Greg Brosman <greg.brosman@gmail.com>
2020-08-13 21:17:39 -0700

    alternate solution

--- 6kyu/high_rank_num.py
+++ 6kyu/high_rank_num.py
@@ -5,16 +5,20 @@
 # [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
 # [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
 
-def highest_rank(arr):
-    counter = 0
-    num = arr[0] 
+
+# def highest_rank(arr):
+#     counter = 0
+#     num = arr[0] 
       
-    for i in arr: 
-        curr_frequency = arr.count(i) 
-        if(curr_frequency > counter): 
-            counter = curr_frequency 
-            num = i 
+#     for i in arr: 
+#         curr_frequency = arr.count(i) 
+#         if(curr_frequency > counter): 
+#             counter = curr_frequency 
+#             num = i 
             
-    return num 
+#     return num 
+
+def highest_rank(arr):
+    return max(sorted(arr,reverse=True), key=arr.count)
 
 print(highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]))
