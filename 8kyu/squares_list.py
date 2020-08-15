71f900c2cda85a0ed4e2c7fac7d817afd7d3ae73
Greg Brosman <greg.brosman@gmail.com>
2020-08-14 20:45:26 -0700

    8kyu squares solved

---
+++ 8kyu/squares_list.py
@@ -0,0 +1,14 @@
+# Complete the square sum function so that it squares each number 
+# passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 
+# 1^2 + 2^2 + 2^2 = 9.
+
+
+def square_sum(numbers):
+    squares = [nums ** 2 for nums in numbers]
+    added = sum(squares)
+    return added
+
+
+print(square_sum([1, 2, 2]))
