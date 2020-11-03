f1e4e5d0166df8b160646c99befb700ece43da2b
Greg Brosman <greg.brosman@gmail.com>
2020-11-03 00:10:38 -0800

    baker.py formatting

--- 5kyu/baker.py
+++ 5kyu/baker.py
@@ -2,12 +2,13 @@
 # Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
 # Examples:
 # # must return 2
-# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
+# cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200})
 # # must return 0
-# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
+# cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000})
 
 
 def cakes(recipe, available_ing):
+    actual_cakes_made = 0
     cakes = []
     for key, value in recipe.items():
         if key in available_ing:
@@ -18,4 +19,4 @@ def cakes(recipe, available_ing):
     return actual_cakes_made
                
 
-print(cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000}))
+print(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}))
